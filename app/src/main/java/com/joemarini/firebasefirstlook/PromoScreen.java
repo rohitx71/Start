f1439aefc969bbefa6649fe26d49021f7b315b22
rohitsingh71 <rohitsingh2359@gmail.com>
2017-07-05 21:26:40 +0530

    Initial commit

---
+++ app/src/main/java/com/joemarini/firebasefirstlook/PromoScreen.java
@@ -0,0 +1,46 @@
+package com.joemarini.firebasefirstlook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class PromoScreen extends AppCompatActivity
+    implements View.OnClickListener {
+
+    private final String TAG = "FB_PROMO";
+    private FirebaseAnalytics mFBAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_promo_screen);
+
+        // TODO: Get a reference to Firebase analytics
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // TODO: Record the user's view of this Activity as a VIEW_ITEM
+        // analytics event, which is provided by Firebase
+        Bundle params = new Bundle();
+        params.putString(FirebaseAnalytics.Param.ITEM_ID, "Promo Item");
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnBuyNow) {
+            // TODO: Record a click on the Buy button as an ECOMMERCE_PURCHASE
+            // Firebase analytics event
+            Bundle params = new Bundle();
+            params.putString(FirebaseAnalytics.Param.ITEM_ID, "Promo Item");
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/joemarini/firebasefirstlook/SignInActivity.java
@@ -0,0 +1,158 @@
+package com.joemarini.firebasefirstlook;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity
+    implements View.OnClickListener{
+    private final String TAG = "FB_SIGNIN";
+
+    // TODO: Add Auth members
+
+
+    private EditText etPass;
+    private EditText etEmail;
+
+    /**
+     * Standard Activity lifecycle methods
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // Set up click handlers and view item references
+        findViewById(R.id.btnCreate).setOnClickListener(this);
+        findViewById(R.id.btnSignIn).setOnClickListener(this);
+        findViewById(R.id.btnSignOut).setOnClickListener(this);
+
+        etEmail = (EditText)findViewById(R.id.etEmailAddr);
+        etPass = (EditText)findViewById(R.id.etPassword);
+
+        // TODO: Get a reference to the Firebase auth object
+
+
+        // TODO: Attach a new AuthListener to detect sign in and out
+
+
+        updateStatus();
+    }
+
+    /**
+     * When the Activity starts and stops, the app needs to connect and
+     * disconnect the AuthListener
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        // TODO: add the AuthListener
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // TODO: Remove the AuthListener
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSignIn:
+                signUserIn();
+                break;
+
+            case R.id.btnCreate:
+                createUserAccount();
+                break;
+
+            case R.id.btnSignOut:
+                signUserOut();
+                break;
+        }
+    }
+
+    private boolean checkFormFields() {
+        String email, password;
+
+        email = etEmail.getText().toString();
+        password = etPass.getText().toString();
+
+        if (email.isEmpty()) {
+            etEmail.setError("Email Required");
+            return false;
+        }
+        if (password.isEmpty()){
+            etPass.setError("Password Required");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void updateStatus() {
+        TextView tvStat = (TextView)findViewById(R.id.tvSignInStatus);
+        // TODO: get the current user
+
+        /*
+        if (user != null) {
+            tvStat.setText("Signed in: " + user.getEmail());
+        }
+        else {
+            tvStat.setText("Signed Out");
+        }
+        */
+    }
+
+    private void updateStatus(String stat) {
+        TextView tvStat = (TextView)findViewById(R.id.tvSignInStatus);
+        tvStat.setText(stat);
+    }
+
+    private void signUserIn() {
+        if (!checkFormFields())
+            return;
+
+        String email = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+
+        // TODO: sign the user in with email and password credentials
+
+
+    }
+
+    private void signUserOut() {
+        // TODO: sign the user out
+
+        updateStatus();
+    }
+
+    private void createUserAccount() {
+        if (!checkFormFields())
+            return;
+
+        String email = etEmail.getText().toString();
+        String password = etPass.getText().toString();
+
+        // TODO: Create the user account
+
+
+    }
+}
